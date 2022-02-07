6516121921f1c29e66e5ec3ba1cdc81e1a6bff39
r-jadhav <renukajadhav2211@gmail.com>
2022-02-07 22:00:35 +0530

    first commit

---
+++ anuation/assets/js/slider.js
@@ -0,0 +1,18 @@
+// $(function() {
+//     $('.testiSlide').bxSlider({
+//       auto: true,
+//       autoControls: true,
+//       speed: 400,
+//       slideSelector: 'div.item',
+      
+//       maxSlides: 3,
+//       moveSlides: 1,
+//       slideWidth: 1100
+  
+  
+//     });
+//   });
+  
+
+
+  
